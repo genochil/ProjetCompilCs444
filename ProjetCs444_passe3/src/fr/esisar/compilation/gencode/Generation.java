2b58f9df0356e4d815c7ec3bf4b0d44cb3b87508
genochil <loic.gen@free.fr>
2018-10-30 11:53:26 +0100

    New_line t write avec string

--- ProjetCs444_passe3/src/fr/esisar/compilation/gencode/Generation.java
+++ ProjetCs444_passe3/src/fr/esisar/compilation/gencode/Generation.java
@@ -2,6 +2,7 @@ package fr.esisar.compilation.gencode;
 
 import fr.esisar.compilation.global.src.*;
 import fr.esisar.compilation.global.src3.*;
+import fr.esisar.compilation.verif.ErreurContext;
 
 /**
  * Génération de code pour un programme JCas à partir d'un arbre décoré.
@@ -9,13 +10,19 @@ import fr.esisar.compilation.global.src3.*;
 
 class Generation {
 
+	// Déclaration registres révservé
+	private Registre R0 = Registre.R0;
+	private Registre R1 = Registre.R1;
+	private Registre R2 = Registre.R2;
+
 	/**
 	 * Méthode principale de génération de code. Génère du code pour l'arbre décoré
 	 * a.
 	 */
+
 	static Prog coder(Arbre a) {
 		Prog.ajouterGrosComment("Programme généré par JCasc");
-		
+
 		Generation gen = new Generation();
 		gen.coder_LISTE_DECL(a.getFils1());
 		gen.coder_LISTE_INST(a.getFils2());
@@ -66,29 +73,54 @@ class Generation {
 			return;
 		case ListeIdent:
 			coder_LISTE_IDF(a.getFils1());
-			// a.getFils2(), stocker valeur variable ?
+			// ajouter valeur de l'indent  en pile
 			return;
 		default:
 		}
 	}
 
 	public void coder_INST(Arbre a) {// Couvi
-		switch(a.getNoeud()) {
+		switch (a.getNoeud()) {
+		case Ecriture:
+			Arbre parcours = a.getFils1();
+			if (parcours.getNoeud() != Noeud.Vide) {
+				coder_INST(parcours.getFils1());
+				NatureType exp_nat_w = parcours.getFils2().getDecor().getType().getNature();
+				switch (exp_nat_w) {
+				case String:
+					Prog.ajouter(
+							Inst.creation1(Operation.WSTR, Operande.creationOpChaine(parcours.getFils2().getChaine())));
+					break;
+				case Interval:
+					break;
+				case Real:
+					break;
+				default:
+					break;
+				}
+			}
+			break;
 		case Ligne:
-			Prog.ajouterComment("newligne, ligne :"+a.getNumLigne());
+			Prog.ajouterComment("newligne, ligne :" + a.getNumLigne());
 			Prog.ajouter(Inst.creation0(Operation.WNL));
-			Prog.ajouterComment("Apres newligne, ligne :"+a.getNumLigne());
+			Prog.ajouterComment("Apres newligne, ligne :" + a.getNumLigne());
 			break;
 		default:
 			break;
-	}
-	}
-
-	public void coder_PLACE(Arbre a) {// Couvi, a faire en premier
-
+		}
 	}
 
-	public void coder_EXP(Arbre a) { // Champey & Clémentin
+	public void coder_PLACE(Arbre a, Registre rc) {// Couvi, a faire en premier
+
+	}
+
+	/*
+	 * Procédure de génération de code Génère du code pour l’expression A tel que
+	 * l’expression soit --évaluée dans le registre Rc. --Précondition: le registre
+	 * Rc est alloué. procedure Coder_Exp (A : Arbre; Rc : Registre)
+	 */
+
+	public void coder_EXP(Arbre a, Registre rc) { // Champey & Clémentin
 
 	}
 
