9be5d4bd0da0b1c754350055e3fc89bc83c3077b
genochil <loic.gen@free.fr>
2018-11-02 11:56:20 +0100

    test stockage variable

--- ProjetCs444_passe3/src/fr/esisar/compilation/gencode/Generation.java
+++ ProjetCs444_passe3/src/fr/esisar/compilation/gencode/Generation.java
@@ -25,6 +25,7 @@ class Generation {
 
 		Generation gen = new Generation();
 		gen.coder_LISTE_DECL(a.getFils1());
+		Prog.ajouter(Inst.creation1(Operation.ADDSP, Operande.creationOpEntier(1)));	
 		gen.coder_LISTE_INST(a.getFils2());
 
 		// Fin du programme
@@ -73,42 +74,55 @@ class Generation {
 			return;
 		case ListeIdent:
 			coder_LISTE_IDF(a.getFils1());
-			// ajouter valeur de l'indent en pile
+			Variable.add_var(a.getFils2());//Ajoute la variable dans notre HashMap, on add le duo : Nom_var, Emplacement
 			return;
 		default:
 		}
 	}
 
-	public void coder_PLACE(Arbre a, Registre rc) {// Couvi, a faire en premier
+	public Operande coder_PLACE(Arbre a) {
+		return Operande.creationOpEntier(15);// Couvi, a faire en premier
 
 	}
 
 	public void coder_INST(Arbre a) {// Couvi
 		switch (a.getNoeud()) {
 		case Ecriture:
+			Prog.ajouterComment("Ecriture, ligne :" + a.getNumLigne());
 			coder_Ecriture(a);
+			Prog.ajouterComment("Fin Ecriture, ligne :" + a.getNumLigne());
 			break;
 		case Ligne:
-			Prog.ajouterComment("newligne, ligne :" + a.getNumLigne());
+			Prog.ajouterComment("NewLigne, ligne :" + a.getNumLigne());
 			Prog.ajouter(Inst.creation0(Operation.WNL));
-			Prog.ajouterComment("Apres newligne, ligne :" + a.getNumLigne());
+			Prog.ajouterComment("Fin NewLigne, ligne :" + a.getNumLigne());
 			break;
 		case Nop:
 			return;
 		case Affect:
+			Prog.ajouterComment("Affect, ligne :" + a.getNumLigne());
 			coder_Affect(a);
+			Prog.ajouterComment("Fin Affect, ligne :" + a.getNumLigne());
 			return;
 		case Pour:
+			Prog.ajouterComment("Pour, ligne :" + a.getNumLigne());
 			coder_Pour(a);
+			Prog.ajouterComment("Fin Pour, ligne :" + a.getNumLigne());
 			return;
 		case TantQue:
+			Prog.ajouterComment("TantQue, ligne :" + a.getNumLigne());
 			coder_TantQue(a);
+			Prog.ajouterComment("Fin TantQue, ligne :" + a.getNumLigne());
 			return;
 		case Si:
+			Prog.ajouterComment("Si, ligne :" + a.getNumLigne());
 			coder_Si(a);
+			Prog.ajouterComment("Fin si, ligne :" + a.getNumLigne());
 			return;
 		case Lecture:
+			Prog.ajouterComment("Lecture, ligne :" + a.getNumLigne());
 			coder_Lecture(a);
+			Prog.ajouterComment("Fin Lecture, ligne :" + a.getNumLigne());
 			return;
 		default:
 			break;
@@ -141,20 +155,46 @@ class Generation {
 			}
 		}
 	}
-
+	/*Pas sur du tout !! */
 	private void coder_Lecture(Arbre a) {
 		// TODO Auto-generated method stub
+		/*
+		Quand on fait un read, on stocke la valeur dans un tableau ou un ident déclaré initialement
+		*/
+		Operande index =coder_PLACE(a.getFils1()); // Ecrire dans index la position en memoire(pile) de ident/tableau contenu dans
+										// a.getFils1, cad ce qui à été déclaré avant le read
+		
+		switch (a.getFils1().getDecor().getType().getNature()) {
+		case Interval:
+			Prog.ajouter(Inst.creation0(Operation.RINT));
+			debord_Interval(a.getFils1(), R0); // verifie qu'il n'y a pas de debordement d'intervalle
+			break;
+		case Real:
+			Prog.ajouter(Inst.creation0(Operation.RFLOAT));
+			break;
+			
+		default:
+			break;
+		}
+		Prog.ajouter(
+				Inst.creation2(Operation.STORE, Operande.opDirect(R1), Operande.creationOpIndexe(0, Registre.GB, index.getRegistre())));
+		// On store la valeur contenue dans R1 qui est la valeur renvoyé par RINT dans la pile à
+		// l'emplacement déterminé par R0
 
+	}
+
+	private void debord_Interval(Arbre a, Registre rc) {
+		// coder en JAVA ou dans le code assembleur ?
 	}
 
 	// fait
 	private void coder_Si(Arbre a) {
-		/*
+		/**
 		 * Le code ci dessous permet de coder un Si en assembleur qui est representé par
 		 * :
 		 * 
-		 * CMP R0 BNE false --code du corps du si-- BRA fin faux : --code du else-- fin
-		 * : --suite du code--
+		 * CMP R0 BNE false --code du corps du si-- BRA fin faux : --code du else-- fin:
+		 * --suite du code--
 		 * 
 		 */
 		Etiq E_sinon = Etiq.nouvelle("False");
@@ -189,7 +229,7 @@ class Generation {
 		Etiq E_debut = Etiq.nouvelle("E_debut");
 		Prog.ajouter(Inst.creation1(Operation.BRA, Operande.creationOpEtiq(E_cond)));
 		Prog.ajouter(E_debut);
-		coder_INST(a.getFils2());
+		coder_LISTE_INST(a.getFils2());
 		Prog.ajouter(E_cond);
 		coder_CMP_BNE(a.getFils1(), 1, E_debut);
 
@@ -199,43 +239,36 @@ class Generation {
 		/*
 		 * int x = ...; for (unsigned i = 0; i < 5; i ++) { x = x + 4; }
 		 * 
-		 * movl $0, %ecx 
-		 * for: 
-		 * cmpl $5, %ecx 
-		 * jae fin_for 
-		 * addl $4, %eax 
-		 * addl $1, %ecx 
-		 * jmp for 
-		 * fin_for:
+		 * movl $0, %ecx for: cmpl $5, %ecx jae fin_for addl $4, %eax addl $1, %ecx jmp
+		 * for fin_for:
 		 */
 		// TODO Auto-generated method stub
 
 		boolean Increment = (a.getFils1().getNoeud().equals(Noeud.Increment));
 		Etiq boucle_for = Etiq.nouvelle("for");
 		Etiq fin_boucle_for = Etiq.nouvelle("fin_for");
-		int val_compteur=0;
-		int val_fin_compteur=0;
+		int val_compteur = 0;
+		int val_fin_compteur = 0;
 		// Recupere valeur fin compteur : a.getFils1.getFils3
 		coder_EXP(a.getFils1().getFils3(), R0);
-		
-		/*Ecrire dans val_fin_compteur la valeur dans R0*/
-		
-		//Recupere valeur debut compteur : a.getFils1.getFils2 
+
+		/* Ecrire dans val_fin_compteur la valeur dans R0 */
+
+		// Recupere valeur debut compteur : a.getFils1.getFils2
 		coder_EXP(a.getFils1().getFils2(), R0); // on met la valeur du debut de compteur dans R0
-		
+
 		Prog.ajouter(boucle_for);
-		
-		Prog.ajouter(
-				Inst.creation2(Operation.CMP, Operande.creationOpEntier(val_fin_compteur), Operande.opDirect(R0)));
+
+		Prog.ajouter(Inst.creation2(Operation.CMP, Operande.creationOpEntier(val_fin_compteur), Operande.opDirect(R0)));
 		if (Increment) {
 			Prog.ajouter(Inst.creation1(Operation.BGE, Operande.creationOpEtiq(fin_boucle_for)));
 		} else {
 			Prog.ajouter(Inst.creation1(Operation.BLE, Operande.creationOpEtiq(fin_boucle_for)));
 		}
-		
+
 		coder_LISTE_INST(a.getFils2());
-		
-		//On incremente ou décremente la valeur du registre
+
+		// On incremente ou décremente la valeur du registre
 		if (Increment) {
 			Prog.ajouter(Inst.creation2(Operation.ADD, Operande.creationOpEntier(1), Operande.opDirect(R0)));
 		} else {
@@ -246,10 +279,12 @@ class Generation {
 
 	private void coder_Affect(Arbre a) {
 		// TODO Auto-generated method stub
-
+		coder_PLACE(a.getFils1());//Emplacement de la variable dans R0
+		coder_EXP(a.getFils2(),R1);//valeur de l'affect dans R1
+		
 	}
 
-	/*
+	/**
 	 * Procédure de génération de code Génère du code pour l’expression A tel que
 	 * l’expression soit --évaluée dans le registre Rc. --Précondition: le registre
 	 * Rc est alloué. procedure Coder_Exp (A : Arbre; Rc : Registre)
@@ -259,10 +294,10 @@ class Generation {
 
 	}
 
-	/*
+	/**
 	 * coder_CMP_BNE : Fonction permettant de coder une condition Avec a l'arbre
-	 * utilisé Val la valeur attendue de la comparaison : 1 - True, 0 - False etiq :
-	 * l'etiquette sur laquelle le branchement est effectué
+	 * utilisé Val la valeur attendue de la comparaison : 1 - True, 0 - False etiq
+	 * :l'etiquette sur laquelle le branchement est effectué
 	 */
 	private void coder_CMP_BNE(Arbre a, int val, Etiq etiq) {
 		coder_EXP(a, R0);
---
+++ ProjetCs444_passe3/src/fr/esisar/compilation/gencode/Variable.java
@@ -0,0 +1,56 @@
+package fr.esisar.compilation.gencode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import fr.esisar.compilation.global.src.Arbre;
+
+public class Variable {
+	
+	private static int taille=0;
+	private static Map<String,Integer> variable=new HashMap<String,Integer>();
+	
+	public static void add_var(Arbre a)
+	{
+		switch(a.getDecor().getType().getNature())
+		{
+		case Boolean:
+		case Interval:
+		case Real:
+			taille++;
+			variable.put(a.getChaine().toLowerCase(), taille);
+			break;
+		case Array:
+			variable.put(a.getChaine().toLowerCase(), taille+1);
+			int size_array=0;
+			//calculer taille du tableau
+			taille+=size_array;
+			break;
+		default:
+			break;
+		}
+	}
+	//renvoie la position de la variable : GB + X
+	public static int get_var(String var)
+	{
+		return variable.get(var); 
+	}
+
+	public static int getTaille() {
+		return taille;
+	}
+
+	public static void setTaille(int taille) {
+		Variable.taille = taille;
+	}
+
+	public static Map<String, Integer> getVariable() {
+		return variable;
+	}
+
+	public static void setVariable(Map<String, Integer> variable) {
+		Variable.variable = variable;
+	}
+	
+	
+}
